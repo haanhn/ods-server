87320a7ccae9c7572dfa1bdb78d959fbc7083052
haanhn <anhnguyenx701@gmail.com>
2020-04-07 04:26:30 +0700

    suggest campaigns

--- models/campaign.js
+++ models/campaign.js
@@ -18,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.DOUBLE,
             allowNull: true
         },
+        campaignStartDate: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
         campaignEndDate: {
             type: DataTypes.DATE,
             allowNull: true
