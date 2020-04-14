48e4c4066a6deeefa1642b90ada89e6ad6f6cf64
CuongNV <48023195+vietcuong122@users.noreply.github.com>
2020-04-14 11:51:45 +0700

    fix deploy

--- app.js
+++ app.js
@@ -81,6 +81,6 @@ models.sequelize
     console.log(err, "Something went wrong with the Database Update!")
   });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   console.log('App listening on port 5000!');
 });
